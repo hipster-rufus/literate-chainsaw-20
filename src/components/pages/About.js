8a00bfe2ac8d26aab55aaa643a9291c081aa2f53
Kali Lott <kalilott@Kalis-MacBook-Pro.local>
2023-08-06 14:19:42 -0600

    Even more fixes to ensure correct deployment

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../styles/Card.css';
 import kali from '../../assets/kali-lott.jpeg'
 
-export default function About() {
+export default function About({ handlePageChange }) {
   return (
     <div className='card'>
       <h1 className='text-center'>About Me</h1>
@@ -15,7 +15,13 @@ export default function About() {
           <div className='col'>
             <p>Hello! My name is Kali Lott, and I am a graphic designer from Denver, CO, who is interested
               in entering the world of programming. <br />
-              I gravitate towards front-end development as a designer, UI design especially, but I have proficiency in both front-end and back-end technologies. <a className="resume-link" href='#resume'>View my resume</a> to see my full list of skills. <br />
+              I gravitate towards front-end development as a designer, UI design especially, but I have proficiency in both front-end and back-end technologies. 
+                <a 
+                  className="resume-link" 
+                  href='#resume' 
+                  onClick={() => handlePageChange('Resume')}
+                >View my resume</a> 
+              to see my full list of skills. <br />
               Being born and raised in Colorado, I enjoy hiking, camping, and water sports! I'm also an avid cinemaphile and boardgame enthusiast. Animals are the best, and I hope one day to have a job flexible enough to take care of a cat or dog -- or both! 
             </p>
           </div>
